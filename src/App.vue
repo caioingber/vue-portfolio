e34ce293d11b7240718766013d31d4dfc20da822
caio_ingber <caioingber@gmail.com>
2020-04-14 12:01:26 -0400

    set up nav

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <navbar />
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
@@ -8,25 +9,26 @@
   </div>
 </template>
 
+<script>
+import Navbar from "./components/Navbar";
+export default {
+  components: { Navbar }
+};
+</script>
+
 <style lang="scss">
+@import "global.scss";
+
+* {
+  box-sizing: border-box;
+  margin: 0;
+  font-family: $main-font;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  color: $primary-color;
 }
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,34 @@
+<template>
+  <nav class="navbar">
+    <div class="navbar__name ">
+      <router-link class="navbar__item" to="/">Caio Ingber</router-link>
+    </div>
+    <div class="navbar__links">
+      <router-link class="navbar__item" to="/">Resume</router-link>
+      <router-link class="navbar__item" to="/about">About</router-link>
+      <router-link class="navbar__item" to="/projects">Projects</router-link>
+      <router-link class="navbar__item" to="/contact">Contact</router-link>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Navbar"
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../global.scss";
+.navbar {
+  @include flex(space-around);
+  height: 100px;
+  &__item {
+  }
+
+  .router-link-active {
+    color: $primary-color;
+    text-decoration: none;
+  }
+}
+</style>
